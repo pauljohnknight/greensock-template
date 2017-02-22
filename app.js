295e688ddeeef5b81d45811c1c2bb3985ef9e59a
Paul Knight <pauljohnknight@gmail.com>
2017-02-22 20:54:11 +0000

    updated js

--- app.js
+++ app.js
@@ -7,9 +7,3 @@ jQuery(document).ready (function($){
     TweenMax.from (box2, 2, {opacity: 0, ease:Power4.easeOut}, .7);
 
 });
-
-
-// fades in homepage after pre-loader (other pages are faded with CSS in style.css)
-var homePage = ('.page-id-7741');
-
-TweenMax.from (homePage, 2, {opacity: 0, ease:Power4.easeOut}, .7);
